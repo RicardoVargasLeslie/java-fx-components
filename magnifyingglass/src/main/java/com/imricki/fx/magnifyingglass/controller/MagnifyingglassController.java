cfa0d56ec38876ddac88ecaf889e93cb912deba6
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-28 17:45:21 +0100

    initial component

---
+++ magnifyingglass/src/main/java/com/imricki/fx/magnifyingglass/controller/MagnifyingglassController.java
@@ -0,0 +1,64 @@
+package com.imricki.fx.magnifyingglass.controller;
+
+import java.io.IOException;
+
+import com.imricki.fx.magnifyingglass.utils.LoaderUtils;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class MagnifyingglassController {
+
+	@FXML
+	private HBox componentView;
+
+	@FXML
+	private Button zoomOutbtn;
+
+	@FXML
+	private Button zoomInbtn;
+
+	@FXML
+	private Button settingsbtn;
+
+	@FXML
+	private Label percentageLabel;
+
+	@FXML
+	private ComboBox<?> viewsCombobox;
+
+	public MagnifyingglassController() throws IOException {
+
+		LoaderUtils.viewLoader("../view/MagnifyingglassView.fxml", this);
+
+	}
+
+	@FXML
+	void onSettings(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onViews(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onZoomIn(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onZoomOut(ActionEvent event) {
+
+	}
+
+	public HBox getComponentView() {
+		return componentView;
+	}
+
+}
---
+++ magnifyingglass/src/main/java/com/imricki/fx/magnifyingglass/main/App.java
@@ -0,0 +1,31 @@
+package com.imricki.fx.magnifyingglass.main;
+
+import com.imricki.fx.magnifyingglass.controller.MagnifyingglassController;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	private String title = "Magnifyingglass";
+	private MagnifyingglassController magnifyingglassController;
+
+	public static void main(String[] args) {
+
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		primaryStage.setTitle(title);
+		primaryStage.setResizable(false);
+		// primaryStage.getIcons().getClass().getResource("/resources/glass.png");
+
+		magnifyingglassController = new MagnifyingglassController();
+		primaryStage.setScene(new Scene(this.magnifyingglassController.getComponentView()));
+		primaryStage.show();
+	}
+
+}
