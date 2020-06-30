2e39fa996a1da62a6e8b6616d00e2f1f82976c3b
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-30 16:57:06 +0100

    setting up

--- vmadministrator/src/main/java/com/imricki/fx/vmadministrator/controller/AdministratorController.java
+++ vmadministrator/src/main/java/com/imricki/fx/vmadministrator/controller/AdministratorController.java
@@ -5,17 +5,26 @@ import java.util.ResourceBundle;
 
 import com.imricki.fx.vmadministrator.utils.LoaderUtils;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 
 public class AdministratorController implements Initializable {
 
+	@FXML
+	private BorderPane viewComponent;
+
 	public AdministratorController() {
 
-		LoaderUtils.viewLoader("", this);
+		LoaderUtils.viewLoader("/fx/vmpanel.fxml", this);
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
+	}
+
+	public BorderPane getViewComponent() {
+		return this.viewComponent;
 	}
 }
--- vmadministrator/src/main/java/com/imricki/fx/vmadministrator/main/App.java
+++ vmadministrator/src/main/java/com/imricki/fx/vmadministrator/main/App.java
@@ -1,29 +1,31 @@
 package com.imricki.fx.vmadministrator.main;
 
+import com.imricki.fx.vmadministrator.controller.AdministratorController;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-	
-	
+
 	/**
 	 * To Run execute mvn compile exec:java
 	 */
-	
+
+	private AdministratorController vmController = new AdministratorController();
+
 	public static void main(String[] args) {
-	
+
 		Application.launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-	
+
 		primaryStage.setTitle("VM Panel");
-		primaryStage.setScene(new Scene(null));
+		primaryStage.setScene(new Scene(this.vmController.getViewComponent()));
 		primaryStage.show();
-		
+
 	}
 
 }
