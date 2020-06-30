4effb689bc8469700abd7fde1920790d5a3ab153
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-30 17:48:36 +0100

    done interface and added icons

--- vmadministrator/src/main/java/com/imricki/fx/vmadministrator/controller/AdministratorController.java
+++ vmadministrator/src/main/java/com/imricki/fx/vmadministrator/controller/AdministratorController.java
@@ -5,14 +5,56 @@ import java.util.ResourceBundle;
 
 import com.imricki.fx.vmadministrator.utils.LoaderUtils;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 public class AdministratorController implements Initializable {
 
 	@FXML
 	private BorderPane viewComponent;
+
+	@FXML
+	private Button settingsbtn;
+
+	@FXML
+	private Button importbtn;
+
+	@FXML
+	private Button exportbtn;
+
+	@FXML
+	private Button newbtn;
+
+	@FXML
+	private Button addbtn;
+
+	@FXML
+	void onAdd(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onExport(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onImport(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onNew(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onSettings(ActionEvent event) {
+
+	}
 
 	public AdministratorController() {
 
