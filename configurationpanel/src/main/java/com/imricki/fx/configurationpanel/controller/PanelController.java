75175dad4d7d68a091ce1ec524db92ce80fe699c
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-02 16:33:54 +0100

    basic view and skeletton

--- configurationpanel/src/main/java/com/imricki/fx/configurationpanel/controller/PanelController.java
+++ configurationpanel/src/main/java/com/imricki/fx/configurationpanel/controller/PanelController.java
@@ -1,5 +1,21 @@
 package com.imricki.fx.configurationpanel.controller;
 
+import com.imricki.fx.configurationpanel.utils.LoaderUtils;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class PanelController {
 
+	@FXML
+	private BorderPane componenetView;
+
+	public PanelController() {
+
+		LoaderUtils.viewLoader("/fxml/panelview.fxml", this);
+	}
+
+	public BorderPane getComponenetView() {
+		return this.componenetView;
+	}
 }
--- configurationpanel/src/main/java/com/imricki/fx/configurationpanel/main/App.java
+++ configurationpanel/src/main/java/com/imricki/fx/configurationpanel/main/App.java
@@ -1,5 +1,24 @@
 package com.imricki.fx.configurationpanel.main;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-}
+public class App extends Application  {
+
+	public static void main(String[] args) {
+
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		primaryStage.setTitle("Config Panel");
+		primaryStage.setScene(new Scene(null));
+		primaryStage.show();
+
+
+	}
+
+}s
